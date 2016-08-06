4b0f066cd509fcacfbf50d728d94139278350c8e
Nawaz <nawazsherif97@gmail.com>
2016-08-06 16:54:18 +0530

    try8

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root "student#new"
+  # root "student#new"
   resources :students
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
